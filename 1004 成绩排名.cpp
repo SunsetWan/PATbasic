4b2157fa58175feee2e99fe3b9d88cdc64de6414
sunset wan <sunsetwan@gmail.com>
2018-02-22 22:41:11 +0800

    no message

--- 1004 成绩排名.cpp
+++ 1004 成绩排名.cpp
@@ -25,29 +25,7 @@ class Student {
 };
 
 void printStuInfo(vector<Student> argu1) {
-	//cout<< argu1.size() << "\n";
-	/*for(int i = 0; i < argu1.size(); i ++) {
-		cout<< argu1[i].sGrade;
-	}*/
-	
-	/*int i = 0;
-	int j = 1;
-	
-	for( ; j < argu1.size(); j ++) {
-		int key = argu1[j].sGrade;
-		i = j - 1;
-		while ( ( i >= 0) && ( (argu1[i].sGrade) > key ) ) {
-			argu1[i+1].sGrade = argu1[i].sGrade;
-			i = i - 1;
-		}
-		argu1[i+1].sGrade = key;
-	}
-	
-	for(auto s : argu1) {
-		cout<< s.sGrade << "\n";
-		
-	}*/
-	int maxGrade = -1;
+		int maxGrade = -1;
 	int minGrade = 101;
 	string maxGraName;
 	int maxGraNum;
@@ -70,18 +48,11 @@ void printStuInfo(vector<Student> argu1) {
 	
 	cout<< maxGraName << " " << maxGraNum << endl;
 	
-	cout<< minGraName << " " << minGraNum << endl;
-	
-	
-	
-	
-	
+	cout<< minGraName << " " << minGraNum << endl;	
 }
 
 int main(int argc, char *argv[]) {
 	int sQuantity;
-	
-	//cout<< "Enter the number:";
 	cin>> sQuantity;
 	
 	vector<Student> vStu;
@@ -93,22 +64,6 @@ int main(int argc, char *argv[]) {
 		cin>> vStu[i].sNum;
 		cin>> vStu[i].sGrade;
 		
-	}
-
-	/*for(int i = 0; i < sQuantity; i ++) {
-			cout<< vStu[i].sName << " ";
-			cout<< vStu[i].sNum << " ";
-			cout<< "\n";
-			//cin>> vStu[i].sGrade;
-		}*/
-	
-	//cout<< vStu.size();
-	printStuInfo(vStu);
-	
-		
-	//Student xiaoming;
-	//int xGrade;
-	//cin>> xGrade;
-	//xiaoming.sGrade = xGrade;
-	//xiaoming.printInfo();
+	}	
+	printStuInfo(vStu);	
 }
