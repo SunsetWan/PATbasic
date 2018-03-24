a065f52e1e1c460f72dc55d0f838bb017196a8c1
sunset wan <sunsetwan@gmail.com>
2018-03-24 19:59:10 +0800

    no message

---
+++ 1019 stringToIntConversion.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "string"
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	string str;
+	int n;
+	str = "1000";
+	n = stoi(str);
+	cout<< n;
+}
---
+++ if statement shorthand.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	int Num, pnum;
+	cin>> Num;
+	pnum = (Num == 1) ? 1 : 0;
+	cout<< pnum;
+	
+	
+}
